d42fd0e32f6069dcccaa567863958b6c2702cb12
Nuthasith Gerdpratoom <nuthasith1997@gmail.com>
2021-03-04 23:49:41 +0700

    4/3/2021

--- script/set_pose.py
+++ script/set_pose.py
@@ -4,30 +4,32 @@ import rospy
 import rospkg
 from gazebo_msgs.msg import ModelState
 from gazebo_msgs.srv import SetModelState
+from nav_msgs.msg import Odometry
 
-def main():
-	rospy.init_node('set_pose')
+class setPose:
+	def __init__(self):
+		rospy.init_node('set_pose')
+		rospy.Subscriber('/mavros/local_position/odom', Odometry, self.set_gazebo_pose)
+		self.state_msg = ModelState()
 
-	state_msg = ModelState()
-	state_msg.model_name = 'apt_prototype'
-	state_msg.pose.position.x = 0
-	state_msg.pose.position.y = 0
-	state_msg.pose.position.z = 0.3
-	state_msg.pose.orientation.x = 0
-	state_msg.pose.orientation.y = 0
-	state_msg.pose.orientation.z = 0
-	state_msg.pose.orientation.w = 0
+	def set_gazebo_pose(self, msg):
+		self.state_msg.model_name = 'apt_prototype'
+		self.state_msg.pose.position.x = msg.pose.pose.position.x
+		self.state_msg.pose.position.y = msg.pose.pose.position.y
+		self.state_msg.pose.position.z = 0 #msg.pose.pose.position.z
+		self.state_msg.pose.orientation.x = msg.pose.pose.orientation.x
+		self.state_msg.pose.orientation.y = msg.pose.pose.orientation.y
+		self.state_msg.pose.orientation.z =	msg.pose.pose.orientation.z
+		self.state_msg.pose.orientation.w =	msg.pose.pose.orientation.w
 
-	rospy.wait_for_service('/gazebo/set_model_state')
-	try:
-		set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
-		resp = set_state( state_msg )
+		rospy.wait_for_service('/gazebo/set_model_state')
+		try:
+			set_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
+			set_state(self.state_msg)
 
-	except rospy.ServiceException, e:
-		print "Service call failed: %s" % e
+		except rospy.ServiceException, e:
+			print "Service call failed: %s" % e
 
 if __name__ == '__main__':
-	try:
-		main()
-	except rospy.ROSInterruptException:
-		pass
+	setPose()
+	rospy.spin()
